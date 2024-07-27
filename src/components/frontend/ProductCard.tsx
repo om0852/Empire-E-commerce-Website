4a91a488054127237f661f4a2326de6bbe735cc6
om salunke <salunkeom474@gmail.com>
2024-07-26 22:43:33 -0700

    sample change in trending product page

--- src/components/frontend/ProductCard.tsx
+++ src/components/frontend/ProductCard.tsx
@@ -36,7 +36,7 @@ const ProductCard: React.FC<IProduct> = ({
   return (
     <div className="border border-gray-200">
       <div className="text-center border-b border-gray-200">
-        <img className="inline-block" src={img} alt={name} />
+        <img className="inline-block w-auto h-[300px]"   src={img} alt={name} />
       </div>
       <div className="px-8 py-4">
         <p className="text-gray-500">{category}</p>
